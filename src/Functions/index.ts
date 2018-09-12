ac738cb5f1d35daee41f97f7a21f32ae3fbe67ea
James McGonegal <jimmymcpeter@users.noreply.github.com>
2018-09-12 11:18:48 -0400

    Fix barrel export default for top level Functions - Issue #6

--- src/Functions/index.ts
+++ src/Functions/index.ts
@@ -1,7 +1,9 @@
-import * as AbstractFunction from "./AbstractFunction";
+export { default as AbstractFunction } from "./AbstractFunction";
+export { default as ApiSessionCreate } from "./ApiSessionCreate";
+export { default as IFunction } from "./IFunction";
+
 import * as AccountsPayable from "./AccountsPayable/index";
 import * as AccountsReceivable from "./AccountsReceivable/index";
-import * as ApiSessionCreate from "./ApiSessionCreate";
 import * as CashManagement from "./CashManagement/index";
 import * as Common from "./Common/index";
 import * as Company from "./Company/index";
@@ -10,17 +12,13 @@ import * as DataDeliveryService from "./DataDeliveryService/index";
 import * as EmployeeExpense from "./EmployeeExpense/index";
 import * as GeneralLedger from "./GeneralLedger/index";
 import * as GlobalConsolidations from "./GlobalConsolidations/index";
-import * as IFunction from "./IFunction";
 import * as InventoryControl from "./InventoryControl/index";
 import * as OrderEntry from "./OrderEntry/index";
 import * as Projects from "./Projects/index";
 import * as Purchasing from "./Purchasing/index";
-
 export {
-    AbstractFunction,
     AccountsPayable,
     AccountsReceivable,
-    ApiSessionCreate,
     CashManagement,
     Common,
     Company,
@@ -29,7 +27,6 @@ export {
     EmployeeExpense,
     GeneralLedger,
     GlobalConsolidations,
-    IFunction,
     InventoryControl,
     OrderEntry,
     Projects,
