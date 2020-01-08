fadef00542fdec80f458878b37a03405668e33e7
Rudy Malhi <rudy@budgeta.com>
2020-01-08 15:23:50 +0200

    Added support for get_companyprefs function

--- src/Functions/Common/index.ts
+++ src/Functions/Common/index.ts
@@ -2,8 +2,7 @@ export { default as Read } from "./Read";
 export { default as ReadByName } from "./ReadByName";
 export { default as ReadByQuery } from "./ReadByQuery";
 export { default as ReadMore } from "./ReadMore";
+export { default as GetCompanyPrefs } from "./GetCompanyPrefs";
 
 import * as Query from "./Query/index";
-export {
-    Query,
-};
+export { Query };
