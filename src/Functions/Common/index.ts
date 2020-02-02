e9169fef935272c68dc51b7b33a45b3ae5a776f0
Rudy Malhi <rudy@budgeta.com>
2020-02-02 09:33:45 +0200

    fixed export of new functions

--- src/Functions/Common/index.ts
+++ src/Functions/Common/index.ts
@@ -3,6 +3,8 @@ export { default as ReadByName } from "./ReadByName";
 export { default as ReadByQuery } from "./ReadByQuery";
 export { default as ReadMore } from "./ReadMore";
 export { default as GetCompanyPrefs } from "./GetCompanyPrefs";
+export { default as GetDimensionRelationships } from "./GetDimensionRelationships";
+export { default as Inspect } from "./Inspect";
 
 import * as Query from "./Query/index";
 export { Query };
