c06691cecd3eaba64ceffcb3689adb9d0b47d462
Rudy Malhi <rudy@budgeta.com>
2020-07-22 12:28:00 +0300

    added support for get_list function

--- src/Functions/Common/index.ts
+++ src/Functions/Common/index.ts
@@ -3,6 +3,7 @@ export { default as ReadByName } from "./ReadByName";
 export { default as ReadByQuery } from "./ReadByQuery";
 export { default as ReadMore } from "./ReadMore";
 export { default as GetCompanyPrefs } from "./GetCompanyPrefs";
+export { default as GetList } from "./GetList";
 export { default as GetDimensions } from "./GetDimensions";
 export { default as GetDimensionRelationships } from "./GetDimensionRelationships";
 export { default as Inspect } from "./Inspect";
---
+++ test/Functions/Common/GetListTest.ts
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2019 Sage Intacct, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "LICENSE" file accompanying this file. This file is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import * as chai from "chai";
+import GetList from "../../../src/Functions/Common/GetList";
+import XmlObjectTestHelper from "../../Xml/XmlObjectTestHelper";
+
+describe("GetList", () => {
+  before(done => {
+    return done();
+  });
+  beforeEach(done => {
+    return done();
+  });
+  afterEach(done => {
+    return done();
+  });
+  after(done => {
+    return done();
+  });
+  it("should get company info", () => {
+    const expected = `<?xml version="1.0" encoding="utf-8" ?>
+<test>
+    <function controlid="unittest">
+        <get_list object="company_info" />
+    </function>
+</test>`;
+
+    const record = new GetList("unittest");
+    record.object = "company_info";
+
+    XmlObjectTestHelper.CompareXml(expected, record);
+  });
+});
